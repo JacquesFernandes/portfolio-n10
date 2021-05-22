30f0b6e3735fa4f2a18aeec585dcbd05fea5e140
Jacques 'Lawrenz' Fernandes <jacques.fernandes.1337@gmail.com>
2021-05-22 16:05:33 +0100

    Plug in NavBar on FullSpec homepage

--- pages/full-spec/index.tsx
+++ pages/full-spec/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import NavBar from "../../components/nav-bar";
 
 export default function FullSpec() {
   return(
-    <div>
-      
+    <div className="h-screen w-screen bg-gradient-to-br from-black to-gray-600" >
+      <NavBar />
     </div>
   );
 }
