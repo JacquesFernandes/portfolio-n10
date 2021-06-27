cb0d75b002ef997d589063e89770eff19c500e54
Jacques 'Lawrenz' Fernandes <jacques.fernandes.1337@gmail.com>
2021-06-27 22:24:18 +0100

    Touch up code on full-spec home

--- pages/full-spec/index.tsx
+++ pages/full-spec/index.tsx
@@ -4,42 +4,49 @@ import Link from "next/link";
 
 export default function FullSpec() {
   return(
-    <div className="h-screen w-screen bg-gradient-to-br from-black to-gray-600" >
+    <div className="min-h-screen w-screen bg-gradient-to-br from-black to-gray-600" >
       <NavBar />
       
       <main
         className="w-4/5 max-w-7xl mx-auto text-white"
       >
         
-        <section id="the-elevator-pitch" >
-          <H2>The Elevator Pitch</H2>
+        <section id="the-elevator-pitch" className='space-y-2' >
+          <H2 >The Elevator Pitch</H2>
           <p>Looking for a full stack developer with hands-on, production experince spanning most contemporary web frameworks?</p>
-          <p>How about someone who can also build physical installations, like a <a href="https://en.wikipedia.org/wiki/Skee-Ball" >skeeball machine</a>?</p>
+          <p>Looking to build physical installations, like a <a href="https://en.wikipedia.org/wiki/Skee-Ball" className="underline" >skeeball machine</a>?</p>
           <p>Or are you just looking for an all-rounder, jack-of-all-trades developer to chill and chat with over a drink?</p>
           <p>If your interest is piqued by any of the above, stick around and find out more!</p>
         </section>
 
-        <H2>A Rough Tour</H2>
+        <H2 className='mb-2' >A Rough Tour</H2>
 
-        <section>
-          <H3>Projects</H3>
-          <p>Excepteur velit et consequat id in adipisicing ad dolor deserunt laborum ea.</p>
-          <Link href="#" ><a>See More...</a></Link>
-        </section>
+        <section className='space-y-4' >
+          <Section>
+            <H3>Projects</H3>
+            <p>Excepteur velit et consequat id in adipisicing ad dolor deserunt laborum ea.</p>
+            <Link href="#" ><a>See More...</a></Link>
+          </Section>
 
-        <section>
-          <H3>Technologies</H3>
-          <p>Ea labore sint est consequat enim enim esse.</p>
-          <Link href="#" ><a>See More...</a></Link>
-        </section>
+          <Section>
+            <H3>Technologies</H3>
+            <p>Ea labore sint est consequat enim enim esse.</p>
+            <Link href="#" ><a>See More...</a></Link>
+          </Section>
 
-        <section>
-          <H3>About Me</H3>
-          <p>Nisi et labore Lorem consequat cupidatat commodo do quis laborum.</p>
-          <Link href="#" ><a>See More...</a></Link>
+          <Section>
+            <H3>About Me</H3>
+            <p>Nisi et labore Lorem consequat cupidatat commodo do quis laborum.</p>
+            <Link href="#" ><a>See More...</a></Link>
+          </Section>
         </section>
 
       </main>
     </div>
   );
 }
+
+const Section: React.FunctionComponent = ({ children }) => 
+  <div className='border-white border-l-2 pl-2' >
+    { children }
+  </div>;
